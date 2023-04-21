9b46dc945ba5ec23b3f169959c14455a1615c2fe
sam-dude <blendert5@gmail.com>
2023-04-21 01:55:40 +0100

    Custom font fix

--- App.js
+++ App.js
@@ -1,10 +1,38 @@
 
-import Home from './screens/Home';
+import { useCallback } from 'react';
+import { Text, View, StyleSheet } from 'react-native';
+import { useFonts } from 'expo-font';
+import * as SplashScreen from 'expo-splash-screen';
+import Home from './screens/Home'
+
+SplashScreen.preventAutoHideAsync();
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+     'nunito-regular': require('./assets/Fonts/Nunito-Regular.ttf'),
+      'nunito-bold': require('./assets/Fonts/Nunito-Bold.ttf')
+  });
+
+  const onLayoutRootView = useCallback(async () => {
+    if (fontsLoaded) {
+      await SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded]);
+
+  if (!fontsLoaded) {
+    return null;
+  }
+
   return (
-    <Home />
+    <View style={styles.container} onLayout={onLayoutRootView}>
+      <Home />
+    </View>
   );
 }
 
-
+const styles = StyleSheet.create({
+  container: {
+      flex: 1,
+      justifyContent: 'center'
+  }
+})
--- screens/Home.js
+++ screens/Home.js
@@ -3,13 +3,18 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function Home() {
     return(
         <View style={styles.container}>
-            <Text>Home Screen</Text>
+            <Text style={styles.home}>Home Screen</Text>
         </View>
     )
 }
 
 const styles = StyleSheet.create({
     container: {
-        padding: 24
+        padding: 24,
+        
+    },
+    home: {
+        fontFamily: 'nunito-regular',
+        fontSize: 40
     }
 })
